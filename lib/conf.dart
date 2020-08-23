1cde82e543777d60229e8bc7dcd217c44a433886
Maxeem <jmax.posh@gmail.com>
2020-08-24 02:04:35 +0300

    user orange accent instead of yellow

--- lib/conf.dart
+++ lib/conf.dart
@@ -7,10 +7,9 @@ const appPlayStoreUrl  = "https://play.google.com/store/apps/details?id=%package
 const appUdacityCourse = 'https://www.udacity.com/course/build-native-mobile-apps-with-flutter--ud905';
 
 const backgroundColor = Color(0xff090909);
-const accentColor1 = Colors.yellowAccent;
+const accentColor1 = Colors.orangeAccent;
 const accentColor2 = Colors.pinkAccent;
 const accentColor3 = Colors.lightBlueAccent;
-const accentColor4 = Colors.orangeAccent;
 
 const backdropHeaderSize   = 0.0;
 const backdropHeaderOffset = 0.0;
